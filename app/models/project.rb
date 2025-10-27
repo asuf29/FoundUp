9dfc96ceb83345ad87202e3546d475bb9257a5be
asuf29 <asuf29@icloud.com>
2025-10-27 17:25:36 +0300

    completed the home page design

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :creator, class_name: "User"
   validates :name, presence: true
-  has_rich_text :description
+  has_rich_text :content
   has_many_attached :images
   has_many_attached :documents
   has_one_attached :video
